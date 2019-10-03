e81c1f2875069e470f00b85d6ff4771504ae628e
Harvey Chan <ch4ch4@gmail.com>
2019-10-03 14:51:39 -0700

    map wip

---
+++ src/pages/Map.tsx
@@ -0,0 +1,46 @@
+import {
+    IonButtons,
+    IonCard,
+    IonCardContent,
+    IonCardHeader,
+    IonCardSubtitle,
+    IonCardTitle,
+    IonContent,
+    IonHeader,
+    IonIcon,
+    IonItem,
+    IonLabel,
+    IonList,
+    IonListHeader,
+    IonMenuButton,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+    IonSearchbar,
+    IonDatetime,
+    IonButton
+    } from '@ionic/react';
+  import { book, build, colorFill, grid } from 'ionicons/icons';
+  import React from 'react';
+  import './Home.css';
+  
+  const HomePage: React.FC = () => {
+    return (
+      <IonPage>
+        <IonHeader>
+          <IonToolbar>
+            <IonButtons slot="start">
+              <IonMenuButton />
+            </IonButtons>
+            <IonTitle>Map</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent>
+            <div id="map_canvas"></div>
+        </IonContent>
+      </IonPage>
+    );
+  };
+  
+  export default HomePage;
+  
